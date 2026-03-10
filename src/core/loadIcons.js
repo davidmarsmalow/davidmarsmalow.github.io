8b653442449f7d90be3a8e86d7efc25e06d1fbdc
David <davidrenaldy1@gmail.com>
2026-03-10 11:51:41 +0700

    Add keycaps SVG icons and update icon loading path in loadIcons function

--- src/core/loadIcons.js
+++ src/core/loadIcons.js
@@ -1,5 +1,5 @@
 export async function loadIcons() {
-  const res = await fetch('/src/assets/icons/keycaps.svg')
+  const res = await fetch('/icons/keycaps.svg')
   const svg = await res.text()
 
   document
